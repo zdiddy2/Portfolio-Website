27a5c8d0468c034ee3ef80a3cb90145dd4cd8bf1
zdiddy2 <z.diddy.2@gmail.com>
2020-10-19 17:44:34 -0600

    Fixing git commands for automatic pull webhook

--- routes/index.js
+++ routes/index.js
@@ -23,10 +23,10 @@ router.post('/payload', jsonParser, function (req, res, next) {
 
   console.log('pulling code from GitHub...');
 
-  exec('git -C /var/www/portfolio reset --hard' +
-      '&& git -C /var/www/portfolio clean -df' + 
-      '&& git -C /var/www/portfolio pull' + 
-      '&& npm install -C /var/www/portfolio install --production', execCallBack);
+  exec('gitreset --hard' +
+      '&& git clean -df' + 
+      '&& git pull' + 
+      '&& npm install --production', execCallBack);
 
   res.sendStatus(200);
   res.end();
