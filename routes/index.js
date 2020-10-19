4efd6d0d6b6667dfe54b91abef133cdd36ac149b
zdiddy2 <z.diddy.2@gmail.com>
2020-10-19 23:04:46 +0100

    Webhook added to routes, uses exec to call git commands

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
+const { exec } = require('child_process');
 var express = require('express');
-var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
@@ -15,7 +15,27 @@ router.get('/about', function (req, res, next) {
   res.render('about.pug');
 });
 
-//test comment
+router.post('/payload', function (req, res, next) {
+  console.log(req.body.pusher.name + 'just pushed to ' + req.body.repository.name);
+
+  console.log('pulling code from GitHub...');
+
+  exec('git -C /var/www/portfolio reset --hard', execCallBack);
+
+  exec('git -C /var/www/portfolio clean -df', execCallBack);
+
+  exec('git -C /var/www/portfolio pull origin master', execCallBack);
+
+  exec('npm install -C /var/www/portfolio install --production', execCallBack);
+
+  res.sendStatus(200);
+  res.end();
+});
+
+function execCallBack(err, stdout, stderr) {
+  if (stdout) console.log(stdout);
+  if (stderr) console.log(stderr);
+}
 
 
 module.exports = router;
